b029d1b2a943e2a94f0fe2d5af3db39f31d16eb6
Felix Lerner <lernerfelix@hotmail.com>
2020-03-26 12:35:34 +0100

    fixed code

--- server/mpyc/demos/sum_server.py
+++ server/mpyc/demos/sum_server.py
@@ -1,23 +1,25 @@
 from mpyc.runtime import mpc
+from timeit import default_timer as timer
 from load_database import load_data
 
 async def main():
-    print("$$$$$$$$$$$$$")
     await mpc.start()
-    print("#############")
     data = load_data('Numbers', 'test')
     flattened_data = [item for sublist in data for item in sublist]
     a = flattened_data[0]
     b = flattened_data[1]
     print("a:", type(a), a)
     print("b:", type(b), b)
+    start = timer()
     result = mpc.add(a, b)
-    print("outputting result of type:", type(result), result)
-    print("OUTPUT:", await mpc.output(result))
+
     print("$$$\n")
     print(await mpc.output(result))
     print("$$$")
-    print("result ouputted")
+
+    end = timer()
+    running_time = end - start
+    print(f'Run time: {running_time}')
 
 if __name__ == '__main__':
     mpc.run(main())
